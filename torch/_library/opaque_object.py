5a8a88b6b96c44d729d049f393520ec3f08fcde7
angelayi <yiangela7@gmail.com>
2025-12-08 09:30:24 -0800

    [opaque obj] Add value-type opaque obj
    ghstack-source-id: f742a84be5e8cee65179361d81f3383ed67f9f0a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167768

--- torch/_library/opaque_object.py
+++ torch/_library/opaque_object.py
@@ -1,3 +1,39 @@
+"""
+Note [Opaque Objects]
+
+Opaque objects are the way we allow custom operators to accept a user-defined
+"black box" object as an input.
+
+There are two kinds of opaque types: VALUE type and REFERENCE type.
+The distinction determines how torch.compile handles the object.
+
+REFERENCE TYPES (default):
+
+Reference-typed opaque objects represent mutable stateful objects and are
+treated as black boxes. In torch.compile, since torch.compile cannot optimize
+the anything (including tensors) within the object, the object must be an
+input to the graph.
+
+You can register a custom class as being a reference-based opaque object class
+through `register_opaque_type(MyClass)`.
+
+VALUE TYPES:
+
+Value-typed opaque objects represent constant values.
+In torch.compile, the graph specializes on the object like how other constants
+are. Therefore there are a couple of methods on the class that must be
+implemented before registering it as a value-typed opaque object class:
+  - __eq__: torch.compile will create guards based on the equality of this
+  object, meaning that a recompilation will happen if __eq__ returns False.
+  - __hash__: This must be implemented for Fake Tensor caching
+  - __repr__: This must be implemented as it will be used in the FX graph's
+    codegen to reconstruct the object. The string representation must be able to
+    construct the object again through its __init__ method.
+
+You can register a custom class as being a reference-based opaque object class
+through `register_opaque_type(MyClass, value_type=True)`.
+"""
+
 from typing import Any, NewType
 
 import torch
@@ -22,7 +58,8 @@ OpaqueTypeStr = "__torch__.torch.classes.aten.OpaqueObject"
 
 OpaqueType = NewType("OpaqueType", torch._C.ScriptObject)
 
-_OPAQUE_TYPES: dict[Any, str] = {}
+# Mapping of type -> (registered string name, whether or not it is a value type)
+_OPAQUE_TYPES: dict[Any, tuple[str, bool]] = {}
 
 
 def get_opaque_type_name(cls: Any) -> str:
@@ -43,10 +80,10 @@ def get_opaque_type_name(cls: Any) -> str:
             f"Class {cls} is not registered as an opaque type. "
             f"Call register_opaque_type({cls.__name__}) first."
         )
-    return _OPAQUE_TYPES[cls]
+    return _OPAQUE_TYPES[cls][0]
 
 
-def register_opaque_type(cls: Any) -> None:
+def register_opaque_type(cls: Any, *, value_type=False) -> None:
     """
     Registers the given type as an opaque type which allows this to be consumed
     by a custom operator.
@@ -56,11 +93,28 @@ def register_opaque_type(cls: Any) -> None:
 
     Args:
         cls (type): The class to register as an opaque type.
+        value_type (bool): Whether or not the opaque type is a value-type or a
+            reference-type. See Note [Opaque Objects] for more details.
     """
+    import torch.utils._pytree as pytree
+
+    # Prevent registration of built-in types (int, str, list, dict, etc.) and torch.Tensor
+    if cls.__module__ == "builtins" or cls is torch.Tensor:
+        raise ValueError(
+            f"Unable to register built-in type {cls} as an opaque type. "
+            "Please wrap it in a custom class and register the custom class as opaque."
+        )
+
+    if cls in pytree.SUPPORTED_NODES:
+        raise ValueError(
+            f"{cls} cannot be registered as an opaque object as it has been "
+            "registered as a pytree. Opaque objects must be pytree leaves."
+        )
+
     # Generate a fully qualified name by combining module and qualname
     name = f"{cls.__module__}.{cls.__qualname__}"
 
-    _OPAQUE_TYPES[cls] = name
+    _OPAQUE_TYPES[cls] = (name, value_type)
 
     torch._C._register_opaque_type(name)
 
@@ -75,4 +129,36 @@ def is_opaque_type(cls: Any) -> bool:
     if cls not in _OPAQUE_TYPES:
         return False
 
-    return torch._C._is_opaque_type_registered(_OPAQUE_TYPES[cls])
+    return torch._C._is_opaque_type_registered(_OPAQUE_TYPES[cls][0])
+
+
+def is_opaque_value_type(cls: Any) -> bool:
+    """
+    Checks if the given type is an opaque **value** type.
+    See Note [Opaque Objects] for more information.
+    """
+    if not is_opaque_type(cls):
+        return False
+
+    if isinstance(cls, str):
+        for cls_str, is_value in _OPAQUE_TYPES.values():
+            if cls_str == cls:
+                return is_value
+
+    return _OPAQUE_TYPES[cls][1]
+
+
+def is_opaque_reference_type(cls: Any) -> bool:
+    """
+    Checks if the given type is an opaque **reference** type.
+    See Note [Opaque Objects] for more information.
+    """
+    if not is_opaque_type(cls):
+        return False
+
+    if isinstance(cls, str):
+        for cls_str, is_value in _OPAQUE_TYPES.values():
+            if cls_str == cls:
+                return not is_value
+
+    return not _OPAQUE_TYPES[cls][1]
