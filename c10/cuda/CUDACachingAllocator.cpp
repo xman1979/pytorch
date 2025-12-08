e1b46f0ec2221e12f7bea58682b288f72f9ac4c0
Ke Wen <kwen@nvidia.com>
2025-12-08 22:28:47 +0000

    [MemPool] Add no-split option
    Add no_split test
    
    ghstack-source-id: 5feda8c380b19288e85fd96d9eaad3a4a6a65d8d
    Pull-Request: https://github.com/pytorch/pytorch/pull/169739

--- c10/cuda/CUDACachingAllocator.cpp
+++ c10/cuda/CUDACachingAllocator.cpp
@@ -1200,6 +1200,9 @@ class DeviceCachingAllocator {
   // tracks which pools we can use as a last resort before ooming
   ska::flat_hash_set<MempoolId_t, MempoolIdHash> use_on_oom_pools;
 
+  // tracks which pools should not split a segment
+  ska::flat_hash_set<MempoolId_t, MempoolIdHash> no_split_pools;
+
   // Map of blocks whose freeing is deferred until after CUDA graph capture.
   //   - Key: Block* to be freed.
   //   - Value: List of "empty nodes" inserted as free markers during capture.
@@ -2574,6 +2577,12 @@ class DeviceCachingAllocator {
     }
   }
 
+  void setNoSplit(MempoolId_t mempool_id) {
+    // Choose if this pool should not split a segment
+    std::lock_guard<std::recursive_mutex> lock(mutex);
+    no_split_pools.insert(mempool_id);
+  }
+
   // See Note [Interaction with CUDA graph capture]
 
   // Called by CUDAGraph::capture_begin
@@ -3047,6 +3056,13 @@ class DeviceCachingAllocator {
       const Block* block,
       size_t size,
       bool is_expandable_segments_active) {
+    // If the pool is marked as not splitting a segment, do not split
+    if (no_split_pools.find(block->pool->owner_MempoolId()) !=
+        no_split_pools.end()) {
+      return false;
+    }
+    // Otherwise, check if the remaining size is greater than the minimum block
+    // size
     size_t remaining = block->size - size;
     if (block->pool->is_small || is_expandable_segments_active) {
       return remaining >= kMinBlockSize;
@@ -4241,6 +4257,11 @@ class NativeCachingAllocator : public CUDAAllocator {
       bool use_on_oom) override {
     assertValidDevice(device);
     device_allocator[device]->setUseOnOOM(std::move(mempool_id), use_on_oom);
+  }
+
+  void setNoSplit(c10::DeviceIndex device, MempoolId_t mempool_id) override {
+    assertValidDevice(device);
+    device_allocator[device]->setNoSplit(std::move(mempool_id));
   }
 
   // CUDAGraph interactions
