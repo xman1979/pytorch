8944b8c4a8bb97b2e19a1b54cc4bb6830aac96f7
angelayi <yiangela7@gmail.com>
2025-12-08 11:38:06 -0800

    [opaque_obj] Replace pytree.register_constant uses
    ghstack-source-id: ba559d0918980c90162437237e1a0d262e2c0f5f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/169846

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -13421,7 +13421,7 @@ graph():
             def forward(self, x, f):
                 return x + f.int_1 + f.int_2
 
-        register_constant(MyInput)
+        torch._library.opaque_object.register_opaque_type(MyInput, value_type=True)
         ep = export(Foo(), (torch.randn(2, 2), MyInput(4, 4)), strict=False)
 
         inp = torch.ones(2, 2)
